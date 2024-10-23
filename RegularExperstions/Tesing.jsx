687af353884bf6de4eec129f2fbf3b5533512c49
vishal <vishal.jagdhane72@gmail.com>
2024-10-23 18:23:27 +0530

    Adding the regular expression

---
+++ RegularExperstions/Tesing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Tesing() {
+  return (
+    <>
+      <Gird xs={12} sm={6} md={6}>
+      <Box sx={{postion:'relective',display:'block',textaline:'center'}}>
+      <Typogrophy ><EidtNoteIcon/>troble Tiket</Typogrophy>
+      <Typogrophy ><LightIcon/>Reuqest call</Typogrophy>
+      <Typogrophy ><userIcon/>Get  Help</Typogrophy>
+      </Box>
+      
+      </Gird>
+    </>
+  )
+}
